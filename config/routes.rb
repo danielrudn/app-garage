99a3135cf7fe20f921e2891a01a149b423fcbdf3
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2018-07-04 13:02:17 -0400

    Add new view for apps#show and search controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :apps
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/search', to: 'search#index'
+
+  resources :apps, only: :show
 end
